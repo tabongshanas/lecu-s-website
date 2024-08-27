4818e4f8199cb087cad80f8f51e1d36aee95e64e
shanasT <shanastabong@gmail.com>
2024-08-27 01:47:41 +0100

    creating shanas page without linking to onther pages javascript is made already

--- home_script.js
+++ home_script.js
@@ -65,3 +65,102 @@ window.addEventListener('keydown' , (e) => {
     rateContainer.classList.add('rate--bring');
   }
 });
+
+
+
+
+const processBtn = document.getElementById('process-btn');
+const cancelBtn = document.getElementById('cancel-btn');
+const shanasBtn = document.getElementById('shanas-btn');
+const proveName = document.querySelector('.prove-name');
+const proveInfo = document.querySelector('.prove-info');
+const proveLayer = document.querySelector('.prove-layer');
+const videoContent = document.querySelector('.video-content');
+const myVideo = document.querySelector('.my-video');
+const videoLayer = document.querySelector('.video-layer');
+
+
+proveInfo.classList.add('prove-info--hidden');
+videoContent.classList.add('video--hidden');
+
+
+shanasBtn.addEventListener('click' , function (e) {
+  e.preventDefault();
+  proveInfo.classList.add('prove-info--bring');
+});
+
+
+window.addEventListener('keydown' , (e) => {
+  e.preventDefault();
+  if (e.key == 'Escape') {
+    proveInfo.classList.add('prove-info--hidden');
+    proveInfo.classList.remove('prove-info--bring');
+
+    // videoContent.classList.add('video--hidden');
+    // videoContent.classList.remove('video--bring');
+  }
+});
+
+
+proveLayer.addEventListener('click' , (e) => {
+  e.preventDefault();
+  proveInfo.classList.add('prove-info--hidden');
+  proveInfo.classList.remove('prove-info--bring');
+});
+
+
+processBtn.addEventListener('click' , (e) => {
+  e.preventDefault();
+
+  const userName = prompt('Please, what is your First name?');
+  if (userName.trim().length >= 6) {
+    // process
+    videoContent.classList.add('video--bring');
+    videoContent.classList.remove('video--hidden');
+
+    setTimeout(function () {
+      alert('Please, play the video.\nThis video is about findind our talent, motivating us in life\nand also inspiring us\nEnjoy!!!');
+    }, 1000);
+
+    setTimeout(function () {
+      // rateContainer.classList.remove('rate--hidden');
+      rateContainer.classList.add('rate--bring');
+      myVideo.pause();
+    }, 10000);
+  }
+  else {
+    alert(`Your name was NOT up too Six (6) characters\nYou enter your name to have ${userName.trim().length} Character(s)\nTry again later`);
+  }
+}); 
+
+
+
+cancelBtn.addEventListener('click' , (e) => {
+  e.preventDefault();
+  proveInfo.classList.add('prove-info--hidden');
+  proveInfo.classList.remove('prove-info--bring');
+})
+
+
+
+videoLayer.addEventListener('click' , (e) => {
+  e.preventDefault();
+  videoContent.classList.add('video--hidden');
+  videoContent.classList.remove('video--bring');
+});
+
+
+
+// playing the video when rate page is close
+rateLatter.addEventListener('click' , function (e) {
+  e.preventDefault();
+  myVideo.play();
+});
+// rateBtn.addEventListener('click' , function (e) {
+//   e.preventDefault();
+//   myVideo.play();
+// });
+rateLayer.addEventListener('click' , function (e) {
+  e.preventDefault();
+  myVideo.play();
+});
